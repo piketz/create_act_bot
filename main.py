04fdc05fabee477358441e3e99466dd9007dea29
piket <expiketz@gmail.com>
2023-11-15 09:01:26 +0500

    add check file fnc on load add null btn

--- main.py
+++ main.py
@@ -33,6 +33,25 @@ downloads_folder = 'downloads'
 generated_folder = 'files'
 max_folder_size = 1000 * 1024 * 1024  # 1000 МБ в байтах
 
+def check_file_content(exelfile):
+    try:
+        # Загрузка данных из файла Excel
+        df = pd.read_excel(exelfile)
+
+        # Проверка наличия конкретных столбцов в файле
+        required_columns = ['Дата создания', 'Конфигурационная единица', 'Объект обслуживания',
+                             'NumberIn', 'Number', 'Задание']
+
+        if not set(required_columns).issubset(set(df.columns)):
+            logging.error(f"File does not contain all required columns: {required_columns}")
+            return False
+
+        logging.info("File content is valid.")
+        return True
+    except Exception as e:
+        logging.error(f"Error checking file content: {e}")
+        return False
+
 
 def generate_documents(exelfile, operation, fio_ispolnitel, day, month, year):
     # Загрузка данных из файла Excel
@@ -127,7 +146,7 @@ def handle_text(message):
 def handle_document(message):
     try:
         if message.document.mime_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-            file_info = bot.get_file(message.document.file_id)
+            file_in = bot.get_file(message.document.file_id)
 
             if get_folder_size(downloads_folder) > max_folder_size:
                 remove_file(downloads_folder)
@@ -136,23 +155,38 @@ def handle_document(message):
 
             file_path = os.path.join(downloads_folder,
                                      datetime.now().strftime('%Y%m%d%H%M%S') + message.document.file_name)
-            downloaded_file = bot.download_file(file_info.file_path)
+            downloaded_file = bot.download_file(file_in.file_path)
 
             if not os.path.exists(downloads_folder):
                 os.makedirs(downloads_folder)
 
-            if file_info.file_size > 20 * 1024 * 1024:
+            if file_in.file_size > 20 * 1024 * 1024:
                 bot.send_message(message.chat.id,
                                  "Размер файла превышает 20MB. Пожалуйста, загрузите файл размером не более 20MB.")
                 return
             else:
                 with open(file_path, 'wb') as new_file:
                     new_file.write(downloaded_file)
-                bot.send_message(message.chat.id, "Excel-файл успешно загружен. Можете начать его обрабатывать.")
+
+                if not check_file_content(file_path):
+                    bot.send_message(message.chat.id, "Excel-файл имеет неверное содержание. "
+                                                      "Он должен содержать столбцы Задание,	"
+                                                      "Описание (RTF),	Адрес,	Объект обслуживания, "
+                                                      "Статус, Крайний срок решения, "
+                                                      "Дата создания, Number,	NumberIn, "
+                                                      "Конфигурационная единица. "
+                                                      "Выберете данные поля при поиске в remo.itsm365.com")
+                    admin_message = f"Получен новый <b>не валидный</> файл от пользователя " \
+                                    f"{message.from_user.username}. filename: <i>{message.document.file_name}</i>"
+                    bot.send_message(admin_chat_id, admin_message, parse_mode='HTML')
+                    return
+                else:
+                    bot.send_message(message.chat.id, "Excel-файл успешно загружен. Можете начать его обрабатывать.")
 
                 # Оповещение администратора о загрузке файла
-                admin_message = f"Получен новый файл от пользователя {message.from_user.username}."
-                bot.send_message(admin_chat_id, admin_message)
+                admin_message = f"Получен новый файл от пользователя {message.from_user.username}. filename: " \
+                                f"<i>{message.document.file_name}</i>"
+                bot.send_message(admin_chat_id, admin_message, parse_mode='HTML')
 
                 # Отправляем клавиатуру с кнопкой "Оставить дату пустой"
                 markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
@@ -175,7 +209,8 @@ def ask_for_date(message, file_path):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
     fn_button = types.KeyboardButton('Замена ФН')
     to_button = types.KeyboardButton('ТО')
-    markup.add(fn_button, to_button)
+    leave_empty_button = types.KeyboardButton('Оставить пусто')
+    markup.add(leave_empty_button, fn_button, to_button)
 
     try:
         if message.text == 'Оставить дату пустой':
@@ -196,66 +231,69 @@ def ask_for_date(message, file_path):
 
 # Функция для запроса operation
 def ask_for_operation(message, file_path, date):
-    operation = message.text
-    if operation == None:
-        bot.register_next_step_handler(message, ask_for_operation, file_path, date)
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    leave_empty_button = types.KeyboardButton('Оставить пусто')
+    markup.add(leave_empty_button)
+
+    if message.text == 'Оставить пусто':
+        operation = ""
     else:
-        # Здесь можно сохранить полученную дату и запросить следующую информацию, например, ФИО
-        bot.send_message(message.chat.id, "Спасибо! Теперь укажите ФИО исполнителя:")
-        bot.register_next_step_handler(message, ask_for_name, file_path, date, operation)
+        operation = message.text
+    bot.send_message(message.chat.id, "Спасибо! Теперь укажите ФИО исполнителя:",reply_markup=markup)
+    bot.register_next_step_handler(message, ask_for_name, file_path, date, operation)
 
 
 # Функция для запроса ФИО
 def ask_for_name(message, file_path, date, operation):
-    fio_ispolnitel = message.text
-    if fio_ispolnitel == None:
-        bot.register_next_step_handler(message, ask_for_name, file_path, date, operation)
-    else:
-        if date:
-            day = None if date.day == '____' else date.day
-            month = None if date.month == '____' else date.month
-            year = None if date.year == '____' else date.year
-        else:
-            day, month, year = None, None, None
 
-        # Здесь можно сохранить полученное ФИО и продолжить обработку файла
+    if date:
+        day = None if date.day == '______' else date.day
+        month = None if date.month == '______' else date.month
+        year = None if date.year == '____' else date.year
+    else:
+        day, month, year = None, None, None
+    if message.text == 'Оставить пусто':
+        fio_ispolnitel = "_____________"
+        bot.send_message(message.chat.id, f"Спасибо! Вы не указали исполнителя. Оказанные услуги: {operation} "
+                                          f"Дата подписания акта: {date} "
+                                          f"Подождите, я создам PDF с данными...")
+    else:
+        fio_ispolnitel = message.text
         bot.send_message(message.chat.id, f"Спасибо! Вы указали ФИО: {fio_ispolnitel}. Оказанные услуги: {operation} "
                                           f"Дата подписания акта: {date} "
                                           f"Подождите, я создам PDF с данными...")
 
-        generated_docs = generate_documents(file_path, operation, fio_ispolnitel, day, month, year)
-        if generated_docs == None:
-            logging.error(f'error format generated_docs = {generated_docs}')
-            bot.send_message(message.chat.id, "Excel-файл имеет неверное содержание. "
-                                              "Он должен содержать столбцы Задание,	"
-                                              "Описание (RTF),	Адрес,	Объект обслуживания, "
-                                              "Статус, Крайний срок решения, "
-                                              "Дата создания, Number,	NumberIn, "
-                                              "Конфигурационная единица. "
-                                              "Выберете данные поля при поиске в remo.itsm365.com")
+    generated_docs = generate_documents(file_path, operation, fio_ispolnitel, day, month, year)
+    if generated_docs == None:
+        logging.error(f'error format generated_docs = {generated_docs}')
+        bot.send_message(message.chat.id, "Excel-файл имеет неверное содержание. "
+                                          "Он должен содержать столбцы Задание,	"
+                                          "Описание (RTF),	Адрес,	Объект обслуживания, "
+                                          "Статус, Крайний срок решения, "
+                                          "Дата создания, Number,	NumberIn, "
+                                          "Конфигурационная единица. "
+                                          "Выберете данные поля при поиске в remo.itsm365.com")
+    else:
+        if not os.path.exists(generated_folder):
+            os.makedirs(generated_folder)
+        merged_pdf_file = os.path.join(generated_folder,
+                                       f"Generated_file_{datetime.now().strftime('%Y%m%d%H%M%S')}.pdf")
+        logging.info('Объединяем все PDF файлы')
+        # Объединяем все PDF файлы
+        merger = PdfMerger()
+        for pdf_path in generated_docs:
+            merger.append(pdf_path)
 
-        else:
+        logging.info('Сохраняем объединенный PDF файл')
+        # Сохраняем объединенный PDF файл
+        merger.write(merged_pdf_file)
+        merger.close()
 
-            if not os.path.exists(generated_folder):
-                os.makedirs(generated_folder)
-            merged_pdf_file = os.path.join(generated_folder,
-                                           f"Generated_file_{datetime.now().strftime('%Y%m%d%H%M%S')}.pdf")
-            logging.info('Объединяем все PDF файлы')
-            # Объединяем все PDF файлы
-            merger = PdfMerger()
-            for pdf_path in generated_docs:
-                merger.append(pdf_path)
-
-            logging.info('Сохраняем объединенный PDF файл')
-            # Сохраняем объединенный PDF файл
-            merger.write(merged_pdf_file)
-            merger.close()
-
-            bot.send_document(message.chat.id, open(merged_pdf_file, 'rb'), caption=f"PDF с данными создан и "
-                                                                                    f"отправлен. Готовы обработать "
-                                                                                    f"ещё один файл?")
-            admin_message = f"Создан файл от пользователя {message.from_user.username}. ФИО: {fio_ispolnitel}"
-            bot.send_document(admin_chat_id, open(merged_pdf_file, 'rb'), caption=admin_message)
+        bot.send_document(message.chat.id, open(merged_pdf_file, 'rb'), caption=f"PDF с данными создан и "
+                                                                                f"отправлен. Готовы обработать "
+                                                                                f"ещё один файл?")
+        admin_message = f"Создан файл от пользователя {message.from_user.username}. ФИО: {fio_ispolnitel}"
+        bot.send_document(admin_chat_id, open(merged_pdf_file, 'rb'), caption=admin_message)
 
 
 def remove_file(folder_path):
